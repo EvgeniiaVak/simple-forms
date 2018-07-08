5c6964b3982ed20592941fddfa91fc68d9d9106c
EvgeniiaVak <27793901+EvgeniiaVak@users.noreply.github.com>
2018-07-08 19:02:21 +0700

    initial commit

---
+++ src/main/java/com/github/evgeniiavak/simpleforms/SimpleFormsApplication.java
@@ -0,0 +1,12 @@
+package com.github.evgeniiavak.simpleforms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleFormsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleFormsApplication.class, args);
+    }
+}
