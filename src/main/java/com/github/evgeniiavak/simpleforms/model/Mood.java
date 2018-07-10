f4f4010cef0feaa6fa52b7926f8affcb56228b1b
EvgeniiaVak <27793901+EvgeniiaVak@users.noreply.github.com>
2018-07-11 06:53:24 +0700

    add lombok

--- src/main/java/com/github/evgeniiavak/simpleforms/model/Mood.java
+++ src/main/java/com/github/evgeniiavak/simpleforms/model/Mood.java
@@ -1,5 +1,6 @@
 package com.github.evgeniiavak.simpleforms.model;
 
+import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.Entity;
@@ -8,6 +9,7 @@ import javax.persistence.Id;
 import java.time.ZonedDateTime;
 import java.util.UUID;
 
+@Data
 @Entity
 public class Mood {
 
@@ -15,53 +17,10 @@ public class Mood {
     @GeneratedValue
     private UUID uuid;
 
-    private int mood;
-
-    private int focus;
-
-    private int anxiety;
-
     @GeneratedValue
     @CreationTimestamp
     private ZonedDateTime time;
 
-    public UUID getUuid() {
-        return uuid;
-    }
+    private int mood, focus, anxiety;
 
-    public void setUuid(UUID uuid) {
-        this.uuid = uuid;
-    }
-
-    public int getMood() {
-        return mood;
-    }
-
-    public void setMood(int mood) {
-        this.mood = mood;
-    }
-
-    public int getFocus() {
-        return focus;
-    }
-
-    public void setFocus(int focus) {
-        this.focus = focus;
-    }
-
-    public ZonedDateTime getTime() {
-        return time;
-    }
-
-    public void setTime(ZonedDateTime time) {
-        this.time = time;
-    }
-
-    public int getAnxiety() {
-        return anxiety;
-    }
-
-    public void setAnxiety(int anxiety) {
-        this.anxiety = anxiety;
-    }
 }
